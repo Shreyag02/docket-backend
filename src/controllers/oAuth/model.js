ea3798a781768e3b6164ed17327cba1b46c92cf1
Shreya Gupta <shreyagupta@Shreya-Gupta-B488s-MacBook-Pro.local>
2022-02-07 19:38:58 +0530

    updated oauth controllers wth try catch

--- src/controllers/oAuth/model.js
+++ src/controllers/oAuth/model.js
@@ -1,110 +1,178 @@
 const { Client, User, Token } = require("../../models");
 const bcrypt = require("bcrypt");
+const {
+  DataForbiddenError,
+  DataNotFoundError,
+} = require("../../utilities/views/errorResponse");
+
+const logger = require("../../services/loggerService");
 
 const VALID_SCOPES = ["read", "write"];
 
 module.exports = {
   getClient: async (clientId, clientSecret) => {
-    const client = await Client.findOne({
-      where: { clientId, archivedAt: null },
-    });
+    try {
+      const client = await Client.findOne({
+        where: {
+          clientId,
+          archivedAt: null,
+        },
+      });
 
-    if (client && client.clientSecret === clientSecret) {
+      if (!client) {
+        throw new DataNotFoundError("Client not found");
+      }
+
+      if (client.clientSecret !== clientSecret) {
+        throw new DataForbiddenError("Client secret is incorrect");
+      }
+
       return {
         id: client.clientId,
         redirectUris: client.dataUris,
         grants: client.grants,
       };
+    } catch (error) {
+      logger.error(error);
+      logger.error(error.stack);
+
+      return false;
     }
   },
 
   getUser: async (username, password) => {
-    const user = await User.findOne({
-      where: { email: username },
-    });
-    let flag = false;
+    try {
+      const user = await User.findOne({
+        where: {
+          email: username,
+          archivedAt: null,
+        },
+      });
 
-    user &&
-      (await bcrypt
+      if (!user) {
+        throw new DataNotFoundError("user not found");
+      }
+
+      let flag = await bcrypt
         .compare(password, user.password)
-        .then(() => {
-          flag = true;
-        })
-        .catch(() => console.log("Incorrect Password")));
+        .then(async (result) => {
+          return result;
+        });
 
-    if (flag) {
-      return { username: user.email, password: user.password, id: user.id };
+      if (!flag) {
+        throw new DataForbiddenError("Incorrect password");
+      }
+
+      return {
+        username: user.email,
+        password: user.password,
+        id: user.id,
+      };
+    } catch (error) {
+      logger.error(error);
+      logger.error(error.stack);
+
+      return false;
     }
   },
 
   saveToken: async (token, client, user) => {
-    const payload = {
-      accessToken: token.accessToken,
-      expiresIn: token.accessTokenExpiresAt,
-      refreshToken: token.refreshToken,
-      refreshTokenExpiresIn: token.refreshTokenExpiresAt,
-      scope: token.scope,
-      tokenType: "Bearer",
-      clientId: client.id,
-      userId: user.id,
-    };
+    try {
+      const payload = {
+        accessToken: token.accessToken,
+        expiresIn: token.accessTokenExpiresAt,
+        refreshToken: token.refreshToken,
+        refreshTokenExpiresIn: token.refreshTokenExpiresAt,
+        scope: token.scope,
+        tokenType: "Bearer",
+        clientId: client.id,
+        userId: user.id,
+      };
 
-    await Token.create(payload);
+      await Token.create(payload);
 
-    return {
-      accessToken: token.accessToken,
-      accessTokenExpiresAt: token.accessTokenExpiresAt,
-      refreshToken: token.refreshToken,
-      scope: token.scope,
-      client: { id: client.clientId },
-      user: { id: user.userId },
-    };
+      return {
+        accessToken: token.accessToken,
+        accessTokenExpiresAt: token.accessTokenExpiresAt,
+        refreshToken: token.refreshToken,
+        scope: token.scope,
+        client: { id: client.clientId },
+        user: { id: user.userId },
+      };
+    } catch (error) {
+      logger.error(error);
+      logger.error(error.stack);
+
+      return false;
+    }
   },
 
   validateScope: (user, client, scope) => {
-    if (!scope.split(" ").every((s) => VALID_SCOPES.indexOf(s) >= 0)) {
+    try {
+      if (!scope.split(" ").every((s) => VALID_SCOPES.indexOf(s) >= 0)) {
+        throw new DataForbiddenError("Invalid scopes");
+      }
+      return scope;
+    } catch (error) {
+      logger.error(error);
+      logger.error(error.stack);
+
       return false;
     }
-    return scope;
   },
 
   revokeToken: async (token) => {
-    const tokenItem = await Token.findOne({
-      where: { refreshToken: token.refreshToken, archivedAt: null },
-    });
+    try {
+      const tokenItem = await Token.findOne({
+        where: { refreshToken: token.refreshToken, archivedAt: null },
+      });
 
-    if (!tokenItem) return false;
-    else {
-      await Token.update(
-        {
-          archivedAt: new Date(),
-        },
-        {
-          where: {
-            refreshToken: tokenItem.refreshToken,
+      if (!tokenItem) {
+        throw new DataForbiddenError("Invalid Token");
+      } else {
+        await Token.update(
+          {
+            archivedAt: new Date(),
           },
-        }
-      );
-      return true;
+          {
+            where: {
+              refreshToken: tokenItem.refreshToken,
+            },
+          }
+        );
+        return true;
+      }
+    } catch (error) {
+      logger.error(error);
+      logger.error(error.stack);
+
+      return false;
     }
   },
 
   getRefreshToken: async (refreshToken) => {
-    const tokenItem = await Token.findOne({
-      where: { refreshToken },
-      include: [
-        {
-          model: User,
-          as: "user",
+    try {
+      const tokenItem = await Token.findOne({
+        where: {
+          refreshToken,
+          archivedAt: null,
         },
-        {
-          model: Client,
-          as: "client",
-        },
-      ],
-    });
+        include: [
+          {
+            model: User,
+            as: "user",
+          },
+          {
+            model: Client,
+            as: "client",
+          },
+        ],
+      });
 
-    if (tokenItem) {
+      if (!tokenItem) {
+        throw new DataForbiddenError("Invalid token");
+      }
+
       return {
         refreshToken: tokenItem.refreshToken,
         refreshTokenExpiresAt: tokenItem.refreshTokenExpiresIn,
@@ -112,25 +180,37 @@ module.exports = {
         client: { id: tokenItem.client.clientId },
         user: { id: tokenItem.user.id },
       };
+    } catch (error) {
+      logger.error(error);
+      logger.error(error.stack);
+
+      return false;
     }
   },
 
   getAccessToken: async (accessToken) => {
-    const tokenItem = await Token.findOne({
-      where: { accessToken },
-      include: [
-        {
-          model: User,
-          as: "user",
+    try {
+      const tokenItem = await Token.findOne({
+        where: {
+          accessToken,
+          archivedAt: null,
         },
-        {
-          model: Client,
-          as: "client",
-        },
-      ],
-    });
+        include: [
+          {
+            model: User,
+            as: "user",
+          },
+          {
+            model: Client,
+            as: "client",
+          },
+        ],
+      });
 
-    if (tokenItem) {
+      if (tokenItem) {
+        throw new DataForbiddenError("Invalid token");
+      }
+
       return {
         accessToken: tokenItem.accessToken,
         accessTokenExpiresAt: tokenItem.expiresIn,
@@ -138,6 +218,11 @@ module.exports = {
         client: { id: tokenItem.client.clientId },
         user: { id: tokenItem.user.id },
       };
+    } catch (error) {
+      logger.error(error);
+      logger.error(error.stack);
+
+      return false;
     }
   },
 };
